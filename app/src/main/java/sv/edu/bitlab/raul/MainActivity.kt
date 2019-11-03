a5165437dbab1853d6b259c60ac99ce1f96bd457
raul <raul.menendez.galan@gmail.com>
2019-11-03 12:03:17 -0600

    prueba de branch

--- app/src/main/java/sv/edu/bitlab/raul/MainActivity.kt
+++ app/src/main/java/sv/edu/bitlab/raul/MainActivity.kt
@@ -37,4 +37,5 @@ class MainActivity : AppCompatActivity(), FormularioFragment.OnFragmentInteracti
         success.replace(R.id.fragment2,containerID).addToBackStack("atras")
         success.commit()
     }
+    //prueba de branch
 }
